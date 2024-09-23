aef462ba2bb5f2159a5e9ab0edbc4d2a696311ad
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2024-09-23 10:00:05 +0530

    Time: 1005 ms (27.83%), Space: 54.2 MB (100%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -6,16 +6,11 @@ class Solution {
         this.random=new Random();
     }   
     public int pick(int target) {
-       int count=0;
-       int res=-1;
-       for(int i=0;i<nums.length;i++){
-        if(nums[i]==target){
-            count++;
-            if(random.nextInt(count)==0){
-                res=i;
-            }
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target)
+                a.add(i);
         }
-       }
-       return res;
+        return a.get(random.nextInt(a.size()));
     }
 }
