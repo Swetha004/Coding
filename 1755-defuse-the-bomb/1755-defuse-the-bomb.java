15c06a9210a38a7898cb666c7e98f24e00d9950c
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2024-09-24 14:46:42 +0530

    Time: 0 ms (100%), Space: 42.2 MB (56.96%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,29 +1,38 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
-        int s=0;
-        int[] a=new int[code.length];
-        if(k==0){
-            return a;
-        }
-        else if(k>0){
-            for(int i=0;i<code.length;i++){
-                s=0;
-                for(int j=1;j<=k;j++){
-                    s=s+code[(i+j)%code.length];
+        int n = code.length;
+        int[] result = new int[n];
+        int sum = 0;
+        int i = 0;
+        if (k > 0) {
+            int j = i + 1;
+            while (i < n) {
+                sum += code[j % n];
+                if (j - i < k) {
+                    j++;
                 }
-                a[i]=s;
+                else if (j - i == k) {
+                    result[i] = sum;
+                    sum -= code[(j - k + 1) % n];
+                    i++;
+                    j++;
+                }
             }
         }
-        else
-        {
-            for(int i=0;i<code.length;i++){
-                s=0;
-                for(int j=1;j<=-1*k;j++){
-                    s=s+code[(i-j+code.length)%code.length];
+        else if (k < 0) {
+            int j = n + (i - Math.abs(k));
+            while (i < n) {
+                sum += code[j];
+                if (j != i) {
+                    j = ++j % n;
                 }
-                a[i]=s;
+                if (j == i) {
+                    result[i] = sum;
+                    sum -= code[(n + (j - Math.abs(k))) % n];
+                    i++;
+                }
             }
         }
-        return a;
+        return result;  
     }
 }
