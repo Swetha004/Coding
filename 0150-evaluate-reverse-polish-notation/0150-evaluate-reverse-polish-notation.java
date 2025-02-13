5cde8e3b0ad5d2b1eebe9735e760fd7280574917
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2025-02-13 15:36:44 +0530

    Time: 7 ms (29.17%), Space: 44.9 MB (41.36%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,24 +1,22 @@
-import java.util.*;
-
 class Solution {
-    public boolean isop(String c) {
-        return c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/");
-    } 
-
+    public boolean isop(String c){
+        return c.equals("+")||c.equals("/")||c.equals("*")||c.equals("-");
+    }
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < tokens.length; i++) {
-            String ch = tokens[i];
-            if (!isop(ch)) {
-                st.push(Integer.parseInt(ch));
-            } else {
-                int n1 = st.pop();
-                int n2 = st.pop();
-                switch (ch) {
-                    case "+": st.push(n2 + n1); break;
-                    case "-": st.push(n2 - n1); break;
-                    case "*": st.push(n2 * n1); break;
-                    case "/": st.push(n2 / n1); break;  // Corrected division order
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<tokens.length;i++){
+            String c=tokens[i];
+            if(!isop(c)){
+                st.push(Integer.parseInt(c));
+            }
+            else{
+                int n1=st.pop();
+                int n2=st.pop();
+                switch(c){
+                    case "+": st.push(n2+n1); break;
+                    case "-": st.push(n2-n1); break;
+                    case "*": st.push(n2*n1); break;
+                    case "/": st.push(n2/n1); break;
                 }
             }
         }
