54dcedd93d7ce493161f5c354f684919277a5eb1
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2025-07-13 23:14:20 +0530

    Time: 0 ms (100%), Space: 44.1 MB (99.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,7 +3,7 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             for(int j=i;j<nums.length;j++){
                 if(nums[j]+nums[j-i]==target){
-                    return new int[]{j-i,j};
+                    return new int [] {j-i,j};
                 }
             }
         }
