9640ee34467f1261cb6de5cf6272f56494859a33
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2025-08-07 18:15:12 +0530

    Time: 9 ms (99.37%), Space: 61.1 MB (9.31%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,10 @@
-class Solution
-{
-    public boolean containsDuplicate(int[] nums)
-    {
-        Arrays.sort(nums);
-        for(int i=1;i<=nums.length-1;i++){
-                if(nums[i]==nums[i-1]){
-                    return true;
-                }
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(!set.add(nums[i])){
+                return true;
+            }
         }
         return false;
     }
