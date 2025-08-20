9794a0423aebc678f08f5ddcc834a8541c2e1101
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2025-08-20 19:43:24 +0530

    Time: 1 ms (99.52%), Space: 57.1 MB (42.76%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,17 +1,15 @@
-class Solution
-{
-    public int maxSubArray(int[] nums)
-    {
-	    int sum=0,cs=nums[0];
-		for(int i=0;i<nums.length;i++){
-		    sum=sum+nums[i];
-		    if(sum>cs){
-		        cs=sum;
-		    }
-		    if(sum<0){
-		        sum=0;
-		    }
-		}
-		return cs;
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum=0,cs=nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            if(sum>cs){
+                cs=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return cs;
     }
 }
