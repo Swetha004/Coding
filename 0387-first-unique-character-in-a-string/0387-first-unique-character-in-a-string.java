58a6d9388f1efd9d0b084a430b109afaaa884728
Swetha004 <126387165+Swetha004@users.noreply.github.com>
2025-08-31 10:31:49 +0530

    Time: 6 ms (80.23%), Space: 45.5 MB (18.72%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -5,7 +5,7 @@ class Solution {
             freq[c-'a']++;
         }
         for(int i=0;i<s.length();i++){
-            if(freq[s.charAt(i) - 'a']==1){
+            if(freq[s.charAt(i)-'a']==1){
                 return i;
             }
         }
